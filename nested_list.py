e62d86ab76b852a539f345cc53e469d9a41dbd9d
Saurabh Salvi <salvisaurabh43@gmail.com>
2021-04-27 12:57:11 -0400

    Update nested_list.py

--- nested_list.py
+++ nested_list.py
@@ -1,37 +1,37 @@
 if __name__ == '__main__':
     
-    records = []
-    second_lowest = 0
-    for _ in range(int(input())):
-        inner_list = []
-        name = input()
-        score = float(input())
-        
-        inner_list.append(name)
-        inner_list.append(score)
-        records.append(inner_list)
-
-    
+  
+  
+    records = [ ['Harsh' , 20] , ['Beria' , 20] , ['Varun' , 19] , ['Kakunami' , 19] , ['Vikas' , 21] ]
 
     for i in range(len(records)):
         for j in range(i , len(records)):
-           if(records[i][1] > records[j][1]):
+            if(records[i][1] > records[j][1]):
                 temp = records[i]
                 records[i] = records[j]
                 records[j] = temp
-                
+
+            if(records[i][1] == records[j][1]):
+                if(records[i][0] > records[j][0]):
+                    temp = records[i]
+                    records[i] = records[j]
+                    records[j] = temp
+    
+    print(records)          
                 
     for sublist in records:
-        sublist[1:] = sorted(sublist[1:])
+        sublist[1:] = sorted(sublist[1:] , reverse=True)
     
-    
+    print(records)
     for i in range(0, len(records) - 1):
         if(records[i][1] != records[i+1][1]):
             second_lowest = records[i+1][1]
             break
-            
-    for sublist in records:
-        if(sublist[1] == second_lowest):
-            print(sublist[0])
+
+
+    if(len(records) > 1 and len(records) < 6):        
+        for sublist in records:
+            if(sublist[1] == second_lowest):
+                print(sublist[0])
 
     
